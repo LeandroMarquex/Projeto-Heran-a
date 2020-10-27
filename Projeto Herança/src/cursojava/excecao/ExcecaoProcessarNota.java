7965aa0e5beb04ed0aebfa18e23721b4f734de52
Leandro Marques <Leandro Marques@SERVER>
2020-10-27 18:28:00 -0300

    classe excecao.

---
+++ Projeto Herança/src/cursojava/excecao/ExcecaoProcessarNota.java
@@ -0,0 +1,10 @@
+package cursojava.excecao;
+
+public class ExcecaoProcessarNota extends Exception {
+	
+	public ExcecaoProcessarNota(String erro) {
+		super("Vixx um erro no processamento do arquivo ao processar as notas do aluno"
+	+ erro);
+	}
+
+}
